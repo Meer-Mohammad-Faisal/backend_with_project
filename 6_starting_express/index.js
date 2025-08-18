2f6b6df531e1fee515d96c5d994c64b0f51c3d14
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-08-19 01:19:12 +0530

    routing in express

--- 6_starting_express/index.js
+++ 6_starting_express/index.js
@@ -2,10 +2,29 @@ const express = require("express");
 
 const app = express(); // creating server
 
-app.use("/",(req,res) => {
-    res.send("i am your home page");
-});
+// // ? for making letter optional
+// app.use("/abou?t",(req,res) => {
+//     res.send({"name":"faisal", "age": 20});
+// });
+
+
+// // + making repeate the letter
+// app.use("/abou+t",(req,res) => {
+//     res.send({"name":"faisal", "age": 20});
+// });
+
+
+// // * you can write anything after the u letter but at the end it should be t.
+// app.use("/abou*t",(req,res) => {
+//     res.send({"name":"faisal", "age": 20});
+// });
+
+
 app.use("/about",(req,res) => {
+    res.send({"name":"faisal", "age": 20});
+});
+app.use("/about/:id/:user",(req,res) => {
+    console.log(req.params); 
     res.send({"name":"faisal", "age": 20});
 });
 app.use("/contact",(req,res) => {
@@ -14,4 +33,10 @@ app.use("/contact",(req,res) => {
 
 app.listen(4000, () => {
     console.log("Listning at port 4000");
+});
+
+// here are default route : it shoul be at the last of all the ohter routes
+// otherwise it get overriden all the route
+app.use("/",(req,res) => {
+    res.send("i am your home page");
 });
