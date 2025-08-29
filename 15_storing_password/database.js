6e9df6b30642baf7dc5370de45480e9300eeb022
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-08-30 02:34:40 +0530

    bcrypt, hahsing and salt

--- 15_storing_password/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-async function main() {
-    await mongoose.connect("mongodb+srv://faisal:Mdbad12o%40%40%40%40%40%40%40@codingadda.najc1k6.mongodb.net/Instagram");
-
-}     
-
-module.exports = main;
